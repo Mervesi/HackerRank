8e4be3afd02bd20781f59799679af5895294735a
Merve <ozermerve61@gmail.com>
2019-04-03 23:57:05 +0300

    day3 added

---
+++ IntrotoConditionalStatements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntrotoConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            if(N % 2 != 0)
+                Console.WriteLine("Weird");
+            else if (N % 2 == 0)
+            {
+                if (2<=N && N<5)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else if (6<=N && N<=20)
+                {
+                    Console.WriteLine("Weird");
+                }
+                else if (N>20)
+                {
+                    Console.WriteLine("Not Weird");
+                }
+            }
+        }
+    }
+}
