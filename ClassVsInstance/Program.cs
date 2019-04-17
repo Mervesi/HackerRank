bb5cc878f71d9159c77c6b367926298d772e3101
Merve <ozermerve61@gmail.com>
2019-04-17 13:14:10 +0300

    day5

---
+++ ClassVsInstance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassVsInstance
+{
+    class Program
+    {
+        public int age;
+        static void Main(string[] args)
+        {
+        }
+
+        //public static void Person(int initialAge)
+        //{
+           
+        //    if (initialAge > 0)
+        //    {
+        //        age = initialAge;
+        //    }
+        //    else
+        //    {
+        //        age = 0;
+        //        Console.WriteLine("Age is not valid, setting age to 0 .");
+        //    }
+        //}
+        //public static void amIOl()
+        //{
+        //    if (age < 13)
+        //    {
+        //        Console.WriteLine("You are young.");
+        //    }
+        //    else if (age >= 13 && age < 18)
+        //    {
+        //        Console.WriteLine("You are a teenager.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("You are old.");
+        //    }
+        //}
+        //public void yearPasses()
+        //{
+        //    age = age + 1;
+        //}
+
+    }
+}
---
+++ LOOPS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n>=2 && n<=20)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(n+" x "+i+" = "+(n*i));
+                }
+            }
+        }
+    }
+}
